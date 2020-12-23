4883e6cf0ec09cd6c2f644342be38a660bdafa2b
Finn Greiter <finn.greiter@gmail.com>
2020-12-23 10:41:31 -0800

    maybe... [ik these rnt vry descriptive]

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lolcats
+
 rainbows!
+
+## usage
+
+`npm i lolcats`
+
+```javascript
+const lol = require('lolcats');
+
+lol('hello', 'this is lolcats!');
+// or
+lol('hello this is lolcats!');
+// or
+lol('hello', 'this', 'is', 'lolcats!');
+```
+
+these all return the same thing
+
+![example](media/screenshot.jpg)
---
+++ index.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk');
+
+const r = chalk.rgb(255, 0, 0);
+const o = chalk.rgb(255, 106, 0);
+const y = chalk.rgb(255, 255, 0);
+const g = chalk.rgb(0, 255, 0);
+const b = chalk.rgb(0, 0, 255);
+const i = chalk.rgb(64, 6, 154);
+const v = chalk.rgb(128, 0, 128);
+
+let colorFns = [r, o, y, g, b, i, v];
+
+const rainbow = (...x) => {
+  let e = x.join(' ').split(' ');
+  let c = { counter: 0, limit: 6 };
+  let n = [];
+  e.forEach(u => {
+    let y = u.split('').map(j => {
+      let d = colorFns[c.counter](j);
+      if (c.counter >= c.limit) c.counter = 0;
+      else c.counter++;
+      return d;
+    });
+    n.push(y.join(''));
+  });
+
+  console.log(n.join(' '));
+};
+
+module.exports = rainbow;
